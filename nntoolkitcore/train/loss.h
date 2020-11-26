ad1c7cc55e336abff00b5e3301dfcf0337f71e86
Oleksii Moiseenko <>
2020-11-26 21:15:48 +0200

    more extern things;

--- nntoolkitcore/train/loss.h
+++ nntoolkitcore/train/loss.h
@@ -10,12 +10,20 @@
 
 #include <stdio.h>
 
-float mean_squared_error(float* y, float * y_pred, int size, int batch);
+#if defined __cplusplus
+extern "C" {
+#endif
 
-void mean_squared_error_derivative(float* y, float * y_pred, float *d_y_pred, int size, int batch);
+float mean_squared_error(float *y, float *y_pred, int size, int batch);
+
+void mean_squared_error_derivative(float *y, float *y_pred, float *d_y_pred, int size, int batch);
 
 float categorical_crossentropy(float *y, float *y_pred, int c, int batch);
 
-void categorical_crossentropy_derivative(float* y, float * y_pred, float *d_y_pred, int c, int batch);
+void categorical_crossentropy_derivative(float *y, float *y_pred, float *d_y_pred, int c, int batch);
+
+#if defined __cplusplus
+}
+#endif
 
 #endif /* File_h */
