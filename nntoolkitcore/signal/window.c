2605b42cc4bfc4aef1626de621c1ad3575c8b328
Oleksii Moiseenko <>
2020-12-16 23:52:47 +0200

    fixed window;

--- nntoolkitcore/signal/window.c
+++ nntoolkitcore/signal/window.c
@@ -12,7 +12,7 @@
 
 void base_hann_family_window(float * vector, int size, int denominator, float alpha){
     for (int i = 0; i < size; ++i){
-        vector[i] = alpha - (1 -  alpha) * cos(2 * M_PI * i / (size - 1));
+        vector[i] = alpha - (1 -  alpha) * cos(2 * M_PI * i / denominator);
     }
 }
 
